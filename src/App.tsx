7133045315a22007f8ef06e5a700038625bda271
Anubhav Singh <anubhavsinghdelhi23@gmail.com>
2023-08-21 15:19:12 +0530

    The Base config setup for MernBazaar frontend

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className={`w-screen h-screen bg-black flex justify-center items-center`}>
+      <div className={`w-96 h-96 bg-white rounded-md `}>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
